0e5f7752b5f07c946544d99f6c71cefea1cc29b3
Ray <774772411@qq.com>
2022-11-24 20:30:22 +0800

    update code

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,85 +1,102 @@
-import { Card, Button, Checkbox, Form, Input } from 'antd';
-import './index.scss';
-import logo from '@/assets/logo-social.png'
+import { Card, Button, Checkbox, Form, Input } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import "./index.scss";
+import logo from "@/assets/logo.jpg";
+import {useStore} from '@/store'
 
 function Login() {
+  const {loginStore} = useStore();
   const onFinish = (values) => {
-    console.log('Success:', values);
+    console.log("Success:", values);
+    loginStore.getToken({
+      mobile:values.username,
+      code:values.password
+    })
+    
   };
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
-  };
+  const onFinishFailed = (err) => {
+    console.log(err);
+  }
 
   return (
-    <div className='login'>
-      <Card className='login-container'>
-        <img className='login-logo' src={logo} alt=''></img>
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt=""></img>
         <Form
-          name="basic"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
+          name="normal_login"
+          className="login-form"
           initialValues={{
             remember: true,
           }}
-          onFinish={onFinish}
           onFinishFailed={onFinishFailed}
-          autoComplete="off"
+          onFinish={onFinish}
+          validateTrigger={["onBlur", "onChange"]}
         >
           <Form.Item
-            label="Username"
             name="username"
             rules={[
               {
                 required: true,
-                message: 'Please input your username!',
+                message: "请输入手机号",
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "请输入正确的手机号",
+                validateTrigger: "onBlur",
               },
             ]}
           >
-            <Input />
+            <Input
+              size="large"
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Phone"
+            />
           </Form.Item>
-
           <Form.Item
-            label="Password"
             name="password"
             rules={[
               {
                 required: true,
-                message: 'Please input your password!',
+                message: "请输入密码",
+              },
+              {
+                len: 6,
+                message: "请输入6位密码",
+                validateTrigger: "onBlur",
               },
             ]}
           >
-            <Input.Password />
+            <Input
+              size="large"
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>记住密码</Checkbox>
+            </Form.Item>
+
+            {/* <a className="login-form-forgot" href="#">
+              Forgot password
+            </a> */}
           </Form.Item>
 
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <Form.Item
-            wrapperCol={{
-              offset: 8,
-              span: 16,
-            }}
-          >
-            <Button type="primary" htmlType="submit">
-              Submit
+          <Form.Item>
+            <Button
+              size="large"
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+            >
+              Log in
             </Button>
           </Form.Item>
         </Form>
       </Card>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
